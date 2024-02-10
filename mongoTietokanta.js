9757badbe95d0dfbc704480774d7c9fb8946676f
Roope Kantola <kantolar@student.uef.fi>
2024-02-10 19:23:36 +0200

    Lisätty alustavia MongoDB mongoose skeemoja fiilispäiväkirjalle ja tietopankille.

---
+++ mongoTietokanta.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const salasana = process.argv[2];
+
+const url =
+  "mongodb+srv://AvustajaAppDev:<password>@avustajaappcluster.gbtovsa.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+mongoose.connect(url);
+
+//Käyttäjän päiväkirjakirjaus
+const paivakirjaKirjausSkeema = new mongoose.Schema({
+  kirjausId: mongoose.Schema.Types.ObjectId,
+  kirjattu: Date,
+  paivanFiilis: Number,
+  sisalto: String,
+});
+
+const PaivakirjaKirjaus = mongoose.model(
+  "PaivakirjaKirjaus",
+  paivakirjaKirjausSkeema
+);
+
+//Käyttäjän fiilispäiväkirjaskeema
+const fiilisPaivakirjaSkeema = new mongoose.Schema({
+  paivakirjaId: mongoose.Schema.Types.ObjectId,
+  kayttaja_id: mongoose.Schema.Types.ObjectId, //Käyttäjän id; muutetaan oikeaan muotoon, kun tiedetään, missä muodossa ovat
+  edellisetFiilikset: [Number],
+  kirjaukset: [mongoose.Schema.Types.ObjectId], //taulukko päiväkirjakirjausten id tunnuksia
+});
+
+const FiilisPaivakirja = mongoose.model(
+  "FiilisPaivakirja",
+  fiilisPaivakirjaSkeema
+);
+
+//Tietopankkiskeema
+const tietopankkiKirjausSkeema = new mongoose.Schema({
+  tietopankkiKirjausId: mongoose.Schema.Types.ObjectId,
+  lisatty: Date,
+  muokattu: Date,
+  lisannyt: kayttaja_id, //HUOM! Tämä muuttunee
+  sisalto: {
+    otsikko: String,
+    leipateksti: String,
+  },
+  linkit: String,
+});
+
+const TietopankkiKirjaus = mongoose.model(
+  "TietopankkiKirjaus",
+  tietopankkiKirjausSkeema
+);
