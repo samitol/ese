006b92b068c441388b2c24fd5405afd61367f2e4
ville.r <101949309+viluuu@users.noreply.github.com>
2024-02-17 22:34:26 +0200

    Luotu etusivun ensimmäinen versio

--- avustaja-app/app/page.js
+++ avustaja-app/app/page.js
@@ -3,24 +3,74 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <h1 className="text-3xl text-white">Logo tähän</h1>
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className="text-xl ">
-            Tervetuloa! Tämä on R1:n ohtu projekti. avustaja-app testi
-          </p>
-          <p> Testi push </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span>
-          </Link>
+    <main className="flex min-h-screen flex-col p-6 bg-white">
+      <div className="flex grow justify-center items-center">
+        <div className="container relative isolate overflow-hidden bg-slate-100 px-6 pt-8 shadow-lg rounded-2xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+          <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+            <div className="flex title-font justify-center lg:justify-start font-semibold items-center text-blue-800">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+                />
+              </svg>
+              <p className="ml-3 text-xl">Avustaja-app</p>
+            </div>
+            <h1 className="mt-8 text-3xl font-bold tracking-tight sm:text-4xl">
+              Aloita käyttämään appia. Verkostoidu muiden avustajien kanssa
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-800">
+              Avustaja appi on tehty parantamaan avustajien arkea.
+              Avustajapankissa on tietopankki, sekä mahdollisuus verkostoitua
+              muiden avustajien kanssa.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+              <Link
+                href="/Kirjaudu"
+                className="rounded-md bg-blue-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              >
+                Kirjaudu sisään
+              </Link>
+              <Link
+                href="/Rekisteroidy"
+                className="text-sm font-semibold leading-6 flex gap-2"
+              >
+                Rekisteröidy
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+              </Link>
+            </div>
+          </div>
+          <div className="relative mt-16 h-80 lg:mt-8">
+            <Image
+              className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+              src={"/hero-example.png"}
+              alt="App screenshot"
+              width={1824}
+              height={1080}
+            />
+          </div>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12"></div>
       </div>
     </main>
   );
