12aded9574d289e001fc4127d66a617cee907cb0
rosaturu <rosaturu@student.uef.fi>
2024-02-19 14:51:18 +0000

    Kirjautumissivun ensimmäinen versio

--- avustaja-app/app/page.js
+++ avustaja-app/app/page.js
@@ -1,26 +1,53 @@
-import Link from "next/link";
-import Image from "next/image";
-
-export default function Page() {
+import React from "react";
+export default function LoginPage() {
+  const InputField = ({ label, type, id, name }) => (
+    <div className="mb-6">
+      <label htmlFor={id} className="block mb-2">
+        {label}:
+      </label>
+      <br />
+      <input
+        type={type}
+        id={id}
+        name={name}
+        className="px-2 py-1 w-full box-border border border-black rounded"
+      />
+    </div>
+  );
+  const Button = ({ onClick, text }) => (
+    <button
+      onClick={onClick}
+      className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer"
+    >
+      {text}
+    </button>
+  );
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <h1 className="text-3xl text-white">Logo tähän</h1>
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className="text-xl ">
-            Tervetuloa! Tämä on R1:n ohtu projekti. avustaja-app
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span>
-          </Link>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100 p-4">
+      <div className="bg-white p-10 border-2 border-black rounded-lg w-full md:w-96">
+        <h1 className="text-2xl font-bold text-center mb-6">
+          <span className="font-bold">Kirjaudu sisään</span>
+        </h1>
+        <form className="text-center mb-6">
+          <InputField label="Sähköposti" type="email" id="email" name="email" />
+          <InputField
+            label="Salasana"
+            type="password"
+            id="password"
+            name="password"
+          />
+          <Button text="Kirjaudu sisään" />
+        </form>
+        <div className="text-center">
+          <a href="#" className="text-blue-500 underline">
+            Ei vielä tunnusta? Rekisteröidy tästä.
+          </a>
+          <br />
+          <a href="#" className="text-gray-600 underline">
+            Unohditko salasanasi?
+          </a>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12"></div>
       </div>
-    </main>
+    </div>
   );
 }
